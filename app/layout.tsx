00a583763c91e1f57001376a9efb81e9184b8166
羽化 <71388811+yuhualhh@users.noreply.github.com>
2024-03-01 07:45:13 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "羽化’s ChatGPT Web",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
